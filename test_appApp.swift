a513345098af08e5ed4ae381c2d0addeae384aca
Scotty <118654680+sdcat404@users.noreply.github.com>
2022-12-13 06:36:29 +0000

    Create test_appApp.swift

---
+++ test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_appApp.swift
+//  test app Watch App
+//
+//  Created by Scott on 12/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct test_app_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
